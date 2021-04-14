25cca0b151441821cd06cd18cfc797be52a69b5c
ongyimeng <ongyimeng@gmail.com>
2021-04-14 23:22:56 +0800

    second commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+
+
+    def index
+        @posts=Post.all.order("created_at DESC")
+    end
+
+    def new
+        @post=current_user.posts.build
+    end
+
+    def create
+        @post=current_user.posts.build(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            
+            render :new
+        end
+       
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body, :user_id)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+    validates :body, :title, presence: true
+
+    belongs_to :user
+    
+end
